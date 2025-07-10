b93c89b95788e99d4f4c6e44513944af8e41d48b
JeonSuYeon <wjstndus75@naver.com>
2025-07-11 01:33:00 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/mygrowth/MyGrowthApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGrowthApplication.class, args);
+    }
+
+}
