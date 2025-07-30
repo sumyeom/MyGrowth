7836c4dfab52b825755367f8abc1656e05f74035
sumyeom <wjstndus75@naver.com>
2025-07-30 13:24:24 +0900

    fix: ErrorCode 오류 수정

--- src/main/java/com/example/mygrowth/global/constant/ErrorCode.java
+++ src/main/java/com/example/mygrowth/global/constant/ErrorCode.java
@@ -45,7 +45,7 @@ public enum ErrorCode {
 
     /* 500 서버 오류 */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"INTERNAL_SERVER_ERROR","서버 오류 입니다."),
-    EMAIL_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EMAIL_SEND_ERROR", "이메일 전송에 문제가 발생했습니다."),
+    EMAIL_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EMAIL_SEND_ERROR", "이메일 전송에 문제가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
